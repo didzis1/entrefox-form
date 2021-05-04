66860c7f5139c381e5042cddc94ff479c0f1bbd9
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-05-04 15:24:44 +0300

    First parts of the questions

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+//import part_1_questions from '../data/part_1_questions.json'
+
+
+const App = () => {
+
+	//const [currentPage, setCurrentPage] = useState(0)
+	//const [answers, setAnswers] = useState({})
+
+
+	return (
+		<div>
+			<form>
+			</form>
+			<button>next</button>
+		</div>
+	)
+}
+
+
+export default App
