f8b706a8d176fd177a435515eea1d44bc0747c90
Didzis Zvaigzne <didzis.zvaigzne@gmail.com>
2021-05-27 13:54:00 +0300

    Refactored answersReducer

--- src/reducers/answersReducer.js
+++ src/reducers/answersReducer.js
@@ -1,68 +1,58 @@
-const initializeSlider = {
-	page: 3,
-	answers: [
-		{
-			id: 7,
-			value: 6,
-		},
-	],
-}
-const reducer = (state = [initializeSlider], action) => {
+import questions from '../data/questions.json'
+
+const initialState = questions.map((page) => {
+	return {
+		page: page.ID,
+		answers: page.questions.map((question) => {
+			switch (question.type) {
+				case 'radio':
+					return {
+						id: question.ID,
+						value: null
+					}
+				case 'text' || 'multi-text':
+					return {
+						id: question.ID,
+						value: ''
+					}
+				case 'date':
+					return {
+						id: question.ID,
+						value: null
+					}
+				default:
+					return {
+						id: question.ID,
+						value: ''
+					}
+			}
+		})
+	}
+})
+
+const reducer = (state = initialState, action) => {
+	console.log(action)
 	switch (action.type) {
 		case 'UPDATE': {
 			const newData = {
 				id: action.data.id,
-				value: action.data.value,
+				value: action.data.value
 			}
-			const pageForData = action.data.page
-
-			// Check if page exists
-			if (state.some((answerSet) => answerSet.page === pageForData)) {
-				return state.map((answersPage) => {
-					if (answersPage.page === pageForData) {
-						// Check if the answer exists in the page
-						if (
-							answersPage.answers.some(
-								(answer) => answer.id === newData.id
-							)
-						) {
-							// If answer is empty filter it out of the state
-							if (!newData || newData.value === '') {
-								return {
-									...answersPage,
-									answers: answersPage.answers.filter(
-										(answer) => answer.id !== newData.id
-									),
-								}
-							}
-							// Answer exists and is not empty => replace it with current answer
-							return {
-								...answersPage,
-								answers: answersPage.answers.map((answer) =>
-									answer.id === newData.id
-										? { ...answer, ...newData }
-										: answer
-								),
-							}
-						}
-						// Answer does not exist in the state current page
-						if (newData.value !== '')
-							return {
-								...answersPage,
-								answers: answersPage.answers.concat(newData),
-							}
+			const dataInPage = action.data.page
+			const newState = state.map((pageToEdit) => {
+				if (pageToEdit.page === dataInPage) {
+					return {
+						...pageToEdit,
+						answers: pageToEdit.answers.map((answer) =>
+							answer.id === newData.id ? newData : answer
+						)
 					}
-					return answersPage
-				})
-			}
-
-			// Page doesn't exist => add with items to state
-			if (newData.value !== '')
-				return state.concat({
-					page: pageForData,
-					answers: [newData],
-				})
-			return state
+				}
+				return {
+					...pageToEdit
+				}
+			})
+			return newState
 		}
 		case 'CLEAR':
 			return []
@@ -77,14 +67,14 @@ export const updateAnswers = (page, id, value) => {
 		data: {
 			page: parseInt(page),
 			id: parseInt(id),
-			value,
-		},
+			value
+		}
 	}
 }
 
 export const clearAnswers = () => {
 	return {
-		type: 'CLEAR',
+		type: 'CLEAR'
 	}
 }
 
