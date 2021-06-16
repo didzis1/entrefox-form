622d8559d05a53aff87cd931b314ad0224c4d710
Jeqqe <business.salonen@gmail.com>
2021-06-16 13:07:55 +0300

    Range data straight from useContext

--- src/components/typeComponents/Range.js
+++ src/components/typeComponents/Range.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { useForm } from '../../contexts/FormContext'
+import { getAnswerByID } from '../../utils'
 
 // Material UI
 import Slider from '@material-ui/core/Slider'
@@ -39,24 +40,23 @@ const CustomSlider = withStyles({
 })(Slider)
 
 const Range = ({ question }) => {
-	const [rangeValue, setRangeValue] = useState(question.choices.max / 2)
 	const { handleInputChange } = useForm()
 	return (
 		<Box my={4}>
 			<CustomSlider
 				aria-labelledby='discrete-slider'
 				valueLabelDisplay='auto'
-				value={rangeValue}
+				value={getAnswerByID(question.page, question.ID)}
 				name={question.ID.toString()}
 				marks={question.marks}
 				min={question.choices.min}
 				max={question.choices.max}
 				step={question.step}
 				// 'event' required!
-				onChange={(event, newValue) => setRangeValue(newValue)}
-				onChangeCommitted={() =>
-					handleInputChange(question.ID, rangeValue)
-				}
+				onChange={(event, newValue) => handleInputChange(
+					question.ID,
+					newValue
+				)}
 				color='primary'
 			/>
 		</Box>
